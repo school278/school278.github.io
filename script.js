af25e7a4247d2373e3c89bd61d883b7988f85c8c
school278 <151072347+school278@users.noreply.github.com>
2023-11-19 21:45:41 +0100

    site de tecnologia

---
+++ script.js
@@ -0,0 +1,17 @@
+/* Set the width of the side navigation to
+250px */
+
+function openNav() {
+  document.getElementById("mySidenav")
+    .style.width = "70%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav")
+    .style.width = "0";
+}
+    // Substitua 'VIDEO_ID' pelo ID do vídeo do YouTube que você deseja incorporar
+    const videoId = 'VIDEO_ID';
+
+    const videoContainer = document.getElementById('video');
+    videoContainer.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
